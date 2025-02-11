af1fcb9cd99c73a664d286d6b46fd4aeceb2d04e
Reda Eskouni <91851906+RedaFarissi@users.noreply.github.com>
2025-02-11 21:42:46 +0100

    Update urls.py
    manual react link

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include , re_path
 from connectWithFront.views import front
 
+from django.views.generic import TemplateView   # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", front , name="front"),
     path('api/', include('api.urls')),    
+
+    # Catch all other routes and serve React index.html
+    # you can go to all route manualy 
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html'), name='react_frontend'),
 ]
